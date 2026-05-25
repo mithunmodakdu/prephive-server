2a08ad512bd72838b64023ee8869fc8ff1c3dc7b
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-05-25 16:20:24 +0600

    uptime, timestamp, cors, express.json, express.urlencoded used

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
 import express, { Request, Response } from 'express';
+import { envVars } from './app/config/env';
+import cors from "cors";
 
 const app = express();
 
+app.use(cors(
+  {
+    origin: "http://localhost:3000",
+    credentials: true
+  }
+))
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-    message: "Welcome to PrepHive Server"
+    message: "Welcome to PrepHive Server",
+    environment: envVars.NODE_ENV,
+    uptime: process.uptime().toFixed(2) + " sec",
+    timestamp: new Date().toISOString()
   })
 })
 
