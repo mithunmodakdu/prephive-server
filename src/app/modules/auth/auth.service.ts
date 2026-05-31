70a43ff314a4ec947ea393344b323c5d2f9edc34
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-05-31 16:19:57 +0600

    generateToken utility function using jwt created

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,32 +1,45 @@
-import { EUserStatus } from "../../../generated/prisma/enums"
-import { prisma } from "../../../lib/prisma"
-import bcrypt from "bcryptjs"
+import { EUserStatus } from "../../../generated/prisma/enums";
+import { prisma } from "../../../lib/prisma";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { generateToken } from "../../../utils/jwt";
+import { envVars } from "../../../config/env";
 
-const login = async(payload: {email: string, password: string}) => {
+const login = async (payload: { email: string; password: string }) => {
   const user = await prisma.user.findUniqueOrThrow({
     where: {
       email: payload.email,
-      status: EUserStatus.ACTIVE
-    }
-  })
+      status: EUserStatus.ACTIVE,
+    },
+  });
 
-  const isCorrectPassword = await bcrypt.compare(payload.password, user.password);
+  const isCorrectPassword = await bcrypt.compare(
+    payload.password,
+    user.password,
+  );
 
-  if(!isCorrectPassword){
-    throw new Error("Your password is not correct.")
+  if (!isCorrectPassword) {
+    throw new Error("Your password is not correct.");
   }
 
-  const accessToken = jwt.sign({email: user.email, role: user.role}, "abcd", {algorithm: "HS256", expiresIn: "1h"});
-  const refreshToken = jwt.sign({email: user.email, role: user.role}, "abcd", {algorithm: "HS256", expiresIn: "90d"});
+  const accessToken = generateToken(
+    { email: user.email, role: user.role },
+    envVars.JWT_ACCESS_SECRET,
+    envVars.JWT_ACCESS_EXPIRE_TIME,
+  );
+  
+  const refreshToken = generateToken(
+    { email: user.email, role: user.role },
+    envVars.JWT_REFRESH_SECRET,
+    envVars.JWT_REFRESH_EXPIRE_TIME,
+  );
 
   return {
     accessToken,
-    refreshToken
-  }
-
-}
+    refreshToken,
+  };
+};
 
 export const AuthService = {
-  login
-}
+  login,
+};
