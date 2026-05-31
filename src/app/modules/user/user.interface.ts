d88326876af841830075526f5f4c7087ae0f9830
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-05-31 22:12:55 +0600

    Interfaces ICreateAdminPayload, ICreateTeacherPayload and zod schema CreateAdminZodSchema, CreateTeacherZodSchema created

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,3 +1,15 @@
+import { EGender } from "../../../generated/prisma/enums";
+
+export interface ICreateAdminPayload {
+  password: string;
+  admin: {
+    email: string;
+    name: string;
+    profilePhoto?: string;
+    contactNumber: string;
+  };
+}
+
 export interface ICreateStudentPayload {
   password: string;
   student: {
@@ -7,4 +19,23 @@ export interface ICreateStudentPayload {
     address?: string;
   };
 }
+
+export interface ICreateTeacherPayload {
+  password: string;
+  teacher: {
+    email: string;
+    name: string;
+    profilePhoto?: string;
+    contactNumber: string;
+    address?: string;
+    experience?: number;
+    gender: EGender;
+    teachingFee: number;
+    qualification: string;
+    currentWorkingPlace: string;
+    designation: string;
+  };
+}
+
+
 
